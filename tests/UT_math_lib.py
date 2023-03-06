87332aaee77a4779c0f4efd322f98213d5395db3
danyleyko <danyleyko.kirill12@gmail.com>
2023-03-06 19:47:28 +0100

    Adding UT for basic math_lib

---
+++ tests/UT_math_lib.py
@@ -0,0 +1,81 @@
+import math
+import unittest
+from src.math_lib import *
+
+
+class TestMathLibrary(unittest.TestCase):
+
+    # Test Method for 'Addition(+)' function
+    def test_add(self):
+        """
+        +
+        """
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-2, 3), 1)
+        self.assertEqual(add(-2, -3), -5)
+
+    # Test Method for 'Subtraction(-)' function
+    def test_sub(self):
+        """
+        -
+        """
+        self.assertEqual(sub(5, 3), 2)
+        self.assertEqual(sub(-2, 3), -5)
+        self.assertEqual(sub(-2, -3), 1)
+
+    # Test Method for 'Multiplication(*)' function
+    def test_multiplication(self):
+        """
+        *
+        """
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(-2, 3), -6)
+        self.assertEqual(mul(-2, -3), 6)
+
+    # Test Method for 'Division(/)' function
+    def test_division(self):
+        """
+        /
+        """
+        self.assertEqual(div(6, 3), 2)
+        self.assertEqual(div(-6, 3), -2)
+        self.assertEqual(div(-6, -3), 2)
+        with self.assertRaises(ZeroDivisionError):
+            div(6, 0)
+
+    # Test Method for 'Square Root(math.sqrt)' function
+    def test_square_root(self):
+        """
+        math.sqrt
+        """
+        self.assertAlmostEqual(sqrt(4), 2)
+        self.assertAlmostEqual(sqrt(9), 3)
+        self.assertAlmostEqual(sqrt(2), math.sqrt(2))
+        with self.assertRaises(ValueError):
+            sqrt(-1)
+
+    # Test Method for 'Power(x^2)' function
+    def test_power(self):
+        """
+        x^2
+        """
+        self.assertEqual(power(2, 3), 8)
+        self.assertEqual(power(-2, 3), -8)
+        self.assertEqual(power(-2, 2), 4)
+        self.assertAlmostEqual(power(2, 0.5), math.sqrt(2))
+        self.assertAlmostEqual(power(2, -1), 0.5)
+
+    # Test Method for 'Factorial(x!)' function
+    def test_factorial(self):
+        """
+        x!
+        """
+        self.assertEqual(factorial(0), 1)
+        self.assertEqual(factorial(1), 1)
+        self.assertEqual(factorial(5), 120)
+        with self.assertRaises(ValueError):
+            factorial(-1)
+
+
+if __name__ == '__main__':
+    unittest.main()
